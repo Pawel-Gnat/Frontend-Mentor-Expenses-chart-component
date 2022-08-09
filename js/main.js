d91f2e43f421d4b0ec2722ba7415f11c1f5ed6de
Paweł <pawel_gnat@o2.pl>
2022-08-09 21:27:16 +0200

    js and css implement

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
+const daysColumns = document.querySelectorAll('.chart__col--data')
+const daysName = document.querySelectorAll('.chart__col--day')
+const popupText = document.querySelectorAll('.popup')
+
 async function displayChart() {
 	const response = await fetch('./data.json')
 	const data = await response.json()
 
 	const days = data.map(a => a.day)
 	const amount = data.map(b => b.amount)
-	console.log(days)
-	console.log(amount)
+
+	daysName.forEach((day, index) => {
+		day.textContent = days[index]
+	})
+
+	daysColumns.forEach((col, param) => {
+		col.style.height = `${amount[param] * 2.2}px`
+	})
+
+	popupText.forEach((pop, value) => {
+		pop.textContent = `$${amount[value]}`
+	})
 }
 
 displayChart()

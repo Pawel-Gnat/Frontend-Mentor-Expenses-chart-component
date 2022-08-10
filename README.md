733d5fdf2f51381130443f43d9dadc9de5f506ba
Paweł <pawel_gnat@o2.pl>
2022-08-10 20:14:48 +0200

    finished js and css

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-In development
+# Frontend Mentor - Expenses chart component solution
+
+This is a solution to the [Expenses chart component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/expenses-chart-component-e7yJBUdjwt). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Links
+
+- Live Site URL: [Frontend Mentor Expenses chart component](https://pawel-gnat.github.io/Frontend-Mentor-Expenses-chart-component/)
+
+## My process
+
+I fetched my json data and insert them in arrays. Later, I used arrays to set height of divs and expense texts. I added a function that set a different background color of a div in relation to a current day.
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- JavaScript
+
+### What I learned
+
+Completed in 8 hours. At first I tried to do it with chart.js but reading the documentation and implementing it was harder than coding that task from zero. I learned to use 2 parameters in forEach loop and new Date function.
+
+## Author
+
+- Frontend Mentor - [@Pawel-Gnat](https://www.frontendmentor.io/profile/Pawel-Gnat)
--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 const daysColumns = document.querySelectorAll('.chart__col--data')
 const daysName = document.querySelectorAll('.chart__col--day')
 const popupText = document.querySelectorAll('.popup')
+const currentDay = new Date().getDay()
 
 async function displayChart() {
 	const response = await fetch('./data.json')
 	const data = await response.json()
-
 	const days = data.map(a => a.day)
 	const amount = data.map(b => b.amount)
 
@@ -20,6 +20,50 @@ async function displayChart() {
 	popupText.forEach((pop, value) => {
 		pop.textContent = `$${amount[value]}`
 	})
-}
 
+	switch (new Date().getDay()) {
+		case 0:
+			daysColumns.forEach(col => {
+				col.classList.remove('active')
+			})
+			daysColumns[6].classList.add('active')
+			break
+		case 1:
+			daysColumns.forEach(col => {
+				col.classList.remove('active')
+			})
+			daysColumns[0].classList.add('active')
+			break
+		case 2:
+			daysColumns.forEach(col => {
+				col.classList.remove('active')
+			})
+			daysColumns[1].classList.add('active')
+			break
+		case 3:
+			daysColumns.forEach(col => {
+				col.classList.remove('active')
+			})
+			daysColumns[2].classList.add('active')
+			break
+		case 4:
+			daysColumns.forEach(col => {
+				col.classList.remove('active')
+			})
+			daysColumns[3].classList.add('active')
+			break
+		case 5:
+			daysColumns.forEach(col => {
+				col.classList.remove('active')
+			})
+			daysColumns[4].classList.add('active')
+			break
+		case 6:
+			daysColumns.forEach(col => {
+				col.classList.remove('active')
+			})
+			daysColumns[5].classList.add('active')
+			break
+	}
+}
 displayChart()
